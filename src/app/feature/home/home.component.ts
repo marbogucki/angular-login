444dd9373015a36924f3ed86176a5e92cf17a38e
Marcin Bogucki <mbogucki@blastlab.co>
2019-05-21 09:53:20 +0200

    add checkbox form

--- src/app/feature/home/home.component.ts
+++ src/app/feature/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  formProducts: FormGroup = this.fb.group({
+    name: 'test',
+    products: []
+  });
+
+  products: Event[] = [];
+  events: Event[];
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.events = [
+      { id: 1, name: 'Toggle this custom checkbox' },
+      { id: 2, name: 'Or toggle this other custom checkbox' },
+      { id: 3, name: 'Check this custom checkbox '}
+    ];
   }
 
+  AddProducts() {
+    console.log(this.formProducts.value);
+  }
+
+  addProduct(event: Event) {
+    this.products = [...this.products, event];
+    console.log(this.products);
+    this.formProducts.patchValue({
+      products: this.products
+    });
+  }
+
+}
+
+export interface Event {
+  id: number;
+  name: string;
 }
