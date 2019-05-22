6f958b3d3fb69f7c21fb2be66819710cbce785c6
Marcin Bogucki <mbogucki@blastlab.co>
2019-05-22 11:56:34 +0200

    add users module and component, add db json

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import {RegisterComponent} from './core/auth/register/register.component';
 
 const routes: Routes = [
   { path: 'home', loadChildren: './feature/home/home.module#HomeModule' },
+  { path: 'users', loadChildren: './feature/users/users.module#UsersModule' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
 ];
---
+++ src/app/feature/users/users-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { UsersComponent } from './users.component';
+
+const usersRoutes: Routes = [
+  {
+    path: '',
+    component: UsersComponent
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(usersRoutes)],
+  exports: [RouterModule]
+})
+export class UsersRoutingModule { }
